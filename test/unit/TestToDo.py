12d391f44892fe6272d88919bee19fe4d3ed5bb5
pserrano9 <93371826+pserrano9@users.noreply.github.com>
2023-05-18 12:36:22 +0200

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -197,6 +197,11 @@ class TestDatabaseFunctions(unittest.TestCase):
         # Testing file functions
         self.assertRaises(TypeError, delete_item("", self.dynamodb))
         print ('End: test_delete_todo_error')
+        
+    def test_improve_coverage(self):
+         os.environ['ENDPOINT_OVERRIDE'] = ''
+         from src.todoList import get_table
+         self.assertRaises(KeyError,get_table(False))
 
 if __name__ == '__main__':
     unittest.main()
