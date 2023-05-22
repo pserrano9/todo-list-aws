7ca67d90b5887455d4fddfed0b79606d1db931de
pserrano9 <pablo.serrano@clarcat.com>
2023-05-22 10:25:37 +0000

    Add Translate function

---
+++ src/translate.py
@@ -0,0 +1,26 @@
+import json
+import decimalencoder
+import todoList
+import boto3
+
+
+def getTranslated(event, context):
+    # create a response
+    item = todoList.get_item(event['pathParameters']['id'])
+
+    if item:
+        translate = boto3.client('translate')
+        language = event['pathParameters']['language']
+        item['text'] =  translate.translate_text(Text=item['text'], SourceLanguageCode="auto", TargetLanguageCode=language)
+        
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(item,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+    else:
+        response = {
+            "statusCode": 404,
+            "body": ""
+        }
+    return response
